2456aec3ec0a6eb37e78c7a5e6d9603b0c44cdc1
AJ Thomas <ajthomas12@msn.com>
2021-01-01 18:20:40 -0700

    Updated settings.

--- blog_app/settings.py
+++ blog_app/settings.py
@@ -21,14 +21,15 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'c080!^y8b_0q=ws$ut0r_#x)prkqu^g$u53f9$-gl@0g!&j84x'
+# SECRET_KEY = 'c080!^y8b_0q=ws$ut0r_#x)prkqu^g$u53f9$-gl@0g!&j84x'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['swdblogapp.herokuapp.com']
 
 
 # Application definition
